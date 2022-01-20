152833080b8202a6d69fa21c4930dbe1eecc5cba
Eric Gonzalez <ejoglez@gmail.com>
2022-01-19 23:56:11 -0600

    commit tree f4704e10a53a317dacbce47f9dcbb5a0e51c089b

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    changeText("This is really cool!");
+})
+
+function changeText (string) {
+   const p = document.querySelector('p');
+   p.innerHTML = string
+
+}
+
+console.log('1st Place');
